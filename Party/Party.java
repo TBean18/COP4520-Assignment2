8ad71149cb00688385396aafcdb3dc5106598412
Taylor Bean <JacobBean18@gmail.com>
2022-02-19 18:44:15 -0500

    init commit

---
+++ Party/Party.java
@@ -0,0 +1,18 @@
+package Party;
+
+public class Party {
+    // Development Notes:
+    /*
+     * The party problem is similar to the prisoner problem disussed in class
+     * One Thread will be the designated CupCake Eater, All others will request a
+     * new CupCake
+     * The Designated eater must keep track of the Number of CupCakes Eaten
+     * Once the numEaten == numThreads we know for sure all threads have entered the
+     * maze
+     * 
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
